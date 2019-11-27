47bf6c55200f47bc4f7a8a6a8943cf7eeb9238c9
rronan <riochet@hotmail.com>
2019-11-27 23:42:30 +0100

    first commit, playing random

--- lichess-bot.py
+++ lichess-bot.py
@@ -1,30 +1,33 @@
 import argparse
-import chess
-from chess.variant import find_variant
-import chess.polyglot
-import engine_wrapper
-import model
 import json
-import lichess
 import logging
 import multiprocessing
-import traceback
-import logging_pool
 import signal
 import sys
 import time
-import backoff
-from config import load_config
-from conversation import Conversation, ChatLine
+import traceback
 from functools import partial
+
+import backoff
+import chess
+import chess.polyglot
+from chess.variant import find_variant
 from requests.exceptions import ChunkedEncodingError, ConnectionError, HTTPError
 from urllib3.exceptions import ProtocolError
+
+import engine_wrapper
+import lichess
+import logging_pool
+import model
 from ColorLogger import enable_color_logging
+from config import load_config
+from conversation import ChatLine, Conversation
 
 logger = logging.getLogger(__name__)
 
 try:
     from http.client import RemoteDisconnected
+
     # New in version 3.5: Previously, BadStatusLine('') was raised.
 except ImportError:
     from http.client import BadStatusLine as RemoteDisconnected
@@ -33,15 +36,19 @@ __version__ = "1.1.4"
 
 terminated = False
 
+
 def signal_handler(signal, frame):
     global terminated
     logger.debug("Recieved SIGINT. Terminating client.")
     terminated = True
 
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 def is_final(exception):
     return isinstance(exception, HTTPError) and exception.response.status_code < 500
+
 
 def upgrade_account(li):
     if li.upgrade_to_bot_account() is None:
@@ -50,46 +57,61 @@ def upgrade_account(li):
     logger.info("Succesfully upgraded to Bot Account!")
     return True
 
+
 @backoff.on_exception(backoff.expo, BaseException, max_time=600, giveup=is_final)
 def watch_control_stream(control_queue, li):
     response = li.get_event_stream()
     try:
         for line in response.iter_lines():
             if line:
-                event = json.loads(line.decode('utf-8'))
+                event = json.loads(line.decode("utf-8"))
                 control_queue.put_nowait(event)
             else:
                 control_queue.put_nowait({"type": "ping"})
-    except (RemoteDisconnected, ChunkedEncodingError, ConnectionError, ProtocolError) as exception:
+    except (
+        RemoteDisconnected,
+        ChunkedEncodingError,
+        ConnectionError,
+        ProtocolError,
+    ) as exception:
         logger.error("Terminating client due to connection error")
         traceback.print_exception(type(exception), exception, exception.__traceback__)
         control_queue.put_nowait({"type": "terminated"})
 
+
 def start(li, user_profile, engine_factory, config):
     challenge_config = config["challenge"]
     max_games = challenge_config.get("concurrency", 1)
-    logger.info("You're now connected to {} and awaiting challenges.".format(config["url"]))
+    logger.info(
+        "You're now connected to {} and awaiting challenges.".format(config["url"])
+    )
     manager = multiprocessing.Manager()
     challenge_queue = manager.list()
     control_queue = manager.Queue()
-    control_stream = multiprocessing.Process(target=watch_control_stream, args=[control_queue, li])
+    control_stream = multiprocessing.Process(
+        target=watch_control_stream, args=[control_queue, li]
+    )
     control_stream.start()
     busy_processes = 0
     queued_processes = 0
 
-    with logging_pool.LoggingPool(max_games+1) as pool:
+    with logging_pool.LoggingPool(max_games + 1) as pool:
         while not terminated:
             event = control_queue.get()
             if event["type"] == "terminated":
                 break
             elif event["type"] == "local_game_done":
                 busy_processes -= 1
-                logger.info("+++ Process Free. Total Queued: {}. Total Used: {}".format(queued_processes, busy_processes))
+                logger.info(
+                    "+++ Process Free. Total Queued: {}. Total Used: {}".format(
+                        queued_processes, busy_processes
+                    )
+                )
             elif event["type"] == "challenge":
                 chlng = model.Challenge(event["challenge"])
                 if chlng.is_supported(challenge_config):
                     challenge_queue.append(chlng)
-                    if (challenge_config.get("sort_by", "best") == "best"):
+                    if challenge_config.get("sort_by", "best") == "best":
                         list_c = list(challenge_queue)
                         list_c.sort(key=lambda c: -c.score())
                         challenge_queue = list_c
@@ -98,26 +120,53 @@ def start(li, user_profile, engine_factory, config):
                         li.decline_challenge(chlng.id)
                         logger.info("    Decline {}".format(chlng))
                     except HTTPError as exception:
-                        if exception.response.status_code != 404: # ignore missing challenge
+                        if (
+                            exception.response.status_code != 404
+                        ):  # ignore missing challenge
                             raise exception
             elif event["type"] == "gameStart":
                 if queued_processes <= 0:
-                    logger.debug("Something went wrong. Game is starting and we don't have a queued process")
+                    logger.debug(
+                        "Something went wrong. Game is starting and we don't have a queued process"
+                    )
                 else:
                     queued_processes -= 1
                 game_id = event["game"]["id"]
-                pool.apply_async(play_game, [li, game_id, control_queue, engine_factory, user_profile, config, challenge_queue])
+                pool.apply_async(
+                    play_game,
+                    [
+                        li,
+                        game_id,
+                        control_queue,
+                        engine_factory,
+                        user_profile,
+                        config,
+                        challenge_queue,
+                    ],
+                )
                 busy_processes += 1
-                logger.info("--- Process Used. Total Queued: {}. Total Used: {}".format(queued_processes, busy_processes))
-            while ((queued_processes + busy_processes) < max_games and challenge_queue): # keep processing the queue until empty or max_games is reached
+                logger.info(
+                    "--- Process Used. Total Queued: {}. Total Used: {}".format(
+                        queued_processes, busy_processes
+                    )
+                )
+            while (
+                queued_processes + busy_processes
+            ) < max_games and challenge_queue:  # keep processing the queue until empty or max_games is reached
                 chlng = challenge_queue.pop(0)
                 try:
                     response = li.accept_challenge(chlng.id)
                     logger.info("    Accept {}".format(chlng))
                     queued_processes += 1
-                    logger.info("--- Process Queue. Total Queued: {}. Total Used: {}".format(queued_processes, busy_processes))
+                    logger.info(
+                        "--- Process Queue. Total Queued: {}. Total Used: {}".format(
+                            queued_processes, busy_processes
+                        )
+                    )
                 except HTTPError as exception:
-                    if exception.response.status_code == 404: # ignore missing challenge
+                    if (
+                        exception.response.status_code == 404
+                    ):  # ignore missing challenge
                         logger.info("    Skip missing {}".format(chlng))
                     else:
                         raise exception
@@ -125,13 +174,21 @@ def start(li, user_profile, engine_factory, config):
     control_stream.terminate()
     control_stream.join()
 
+
 @backoff.on_exception(backoff.expo, BaseException, max_time=600, giveup=is_final)
-def play_game(li, game_id, control_queue, engine_factory, user_profile, config, challenge_queue):
+def play_game(
+    li, game_id, control_queue, engine_factory, user_profile, config, challenge_queue
+):
     response = li.get_game_stream(game_id)
     lines = response.iter_lines()
 
-    #Initial response of stream will be the full game info. Store it
-    game = model.Game(json.loads(next(lines).decode('utf-8')), user_profile["username"], li.baseUrl, config.get("abort_time", 20))
+    # Initial response of stream will be the full game info. Store it
+    game = model.Game(
+        json.loads(next(lines).decode("utf-8")),
+        user_profile["username"],
+        li.baseUrl,
+        config.get("abort_time", 20),
+    )
     board = setup_board(game)
     engine = engine_factory(board)
     conversation = Conversation(game, engine, li, __version__, challenge_queue)
@@ -143,13 +200,15 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     book_cfg = polyglot_cfg.get("book", {})
 
     try:
-        if not polyglot_cfg.get("enabled") or not play_first_book_move(game, engine, board, li, book_cfg):
+        if not polyglot_cfg.get("enabled") or not play_first_book_move(
+            game, engine, board, li, book_cfg
+        ):
             play_first_move(game, engine, board, li)
 
         engine.set_time_control(game)
 
         for binary_chunk in lines:
-            upd = json.loads(binary_chunk.decode('utf-8')) if binary_chunk else None
+            upd = json.loads(binary_chunk.decode("utf-8")) if binary_chunk else None
             u_type = upd["type"] if upd else "ping"
             if u_type == "chatLine":
                 conversation.react(ChatLine(upd), game)
@@ -159,20 +218,29 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
                 board = update_board(board, moves[-1])
                 if not board.is_game_over() and is_engine_move(game, moves):
                     if config.get("fake_think_time") and len(moves) > 9:
-                        delay = min(game.clock_initial, game.my_remaining_seconds()) * 0.015
+                        delay = (
+                            min(game.clock_initial, game.my_remaining_seconds()) * 0.015
+                        )
                         accel = 1 - max(0, min(100, len(moves) - 20)) / 150
                         sleep = min(5, delay * accel)
                         time.sleep(sleep)
                     best_move = None
-                    if polyglot_cfg.get("enabled") and len(moves) <= polyglot_cfg.get("max_depth", 8) * 2 - 1:
+                    if (
+                        polyglot_cfg.get("enabled")
+                        and len(moves) <= polyglot_cfg.get("max_depth", 8) * 2 - 1
+                    ):
                         best_move = get_book_move(board, book_cfg)
                     if best_move == None:
-                        best_move = engine.search(board, upd["wtime"], upd["btime"], upd["winc"], upd["binc"])
+                        best_move = engine.search(
+                            board, upd["wtime"], upd["btime"], upd["winc"], upd["binc"]
+                        )
                     li.make_move(game.id, best_move)
                     game.abort_in(config.get("abort_time", 20))
             elif u_type == "ping":
                 if game.should_abort_now():
-                    logger.info("    Aborting {} by lack of activity".format(game.url()))
+                    logger.info(
+                        "    Aborting {} by lack of activity".format(game.url())
+                    )
                     li.abort(game.id)
     except HTTPError as e:
         ongoing_games = li.get_ongoing_games()
@@ -182,8 +250,13 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
                 game_over = False
                 break
         if not game_over:
-            logger.warn("Abandoning game due to HTTP "+response.status_code)
-    except (RemoteDisconnected, ChunkedEncodingError, ConnectionError, ProtocolError) as exception:
+            logger.warn("Abandoning game due to HTTP " + response.status_code)
+    except (
+        RemoteDisconnected,
+        ChunkedEncodingError,
+        ConnectionError,
+        ProtocolError,
+    ) as exception:
         logger.error("Abandoning game due to connection error")
         traceback.print_exception(type(exception), exception, exception.__traceback__)
     finally:
@@ -231,9 +304,13 @@ def get_book_move(board, config):
             if selection == "weighted_random":
                 move = reader.weighted_choice(board).move()
             elif selection == "uniform_random":
-                move = reader.choice(board, minimum_weight=config.get("min_weight", 1)).move()
+                move = reader.choice(
+                    board, minimum_weight=config.get("min_weight", 1)
+                ).move()
             elif selection == "best_move":
-                move = reader.find(board, minimum_weight=config.get("min_weight", 1)).move()
+                move = reader.find(
+                    board, minimum_weight=config.get("min_weight", 1)
+                ).move()
         except IndexError:
             # python-chess raises "IndexError" if no entries found
             move = None
@@ -272,25 +349,45 @@ def update_board(board, move):
     board.push(uci_move)
     return board
 
+
 def intro():
-    return r"""
+    return (
+        r"""
     .   _/|
     .  // o\
     .  || ._)  lichess-bot %s
     .  //__\
     .  )___(   Play on Lichess with a bot
-    """ % __version__
+    """
+        % __version__
+    )
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Play on Lichess with a bot')
-    parser.add_argument('-u', action='store_true', help='Add this flag to upgrade your account to a bot account.')
-    parser.add_argument('-v', action='store_true', help='Verbose output. Changes log level from INFO to DEBUG.')
-    parser.add_argument('--config', help='Specify a configuration file (defaults to ./config.yml)')
-    parser.add_argument('-l', '--logfile', help="Log file to append logs to.", default=None)
+    parser = argparse.ArgumentParser(description="Play on Lichess with a bot")
+    parser.add_argument(
+        "-u",
+        action="store_true",
+        help="Add this flag to upgrade your account to a bot account.",
+    )
+    parser.add_argument(
+        "-v",
+        action="store_true",
+        help="Verbose output. Changes log level from INFO to DEBUG.",
+    )
+    parser.add_argument(
+        "--config", help="Specify a configuration file (defaults to ./config.yml)"
+    )
+    parser.add_argument(
+        "-l", "--logfile", help="Log file to append logs to.", default=None
+    )
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG if args.v else logging.INFO, filename=args.logfile,
-                        format="%(asctime)-15s: %(message)s")
+    logging.basicConfig(
+        level=logging.DEBUG if args.v else logging.INFO,
+        filename=args.logfile,
+        format="%(asctime)-15s: %(message)s",
+    )
     enable_color_logging(debug_lvl=logging.DEBUG if args.v else logging.INFO)
     logger.info(intro())
     CONFIG = load_config(args.config or "./config.yml")
@@ -308,4 +405,8 @@ if __name__ == "__main__":
         engine_factory = partial(engine_wrapper.create_engine, CONFIG)
         start(li, user_profile, engine_factory, CONFIG)
     else:
-        logger.error("{} is not a bot account. Please upgrade it to a bot account!".format(user_profile["username"]))
+        logger.error(
+            "{} is not a bot account. Please upgrade it to a bot account!".format(
+                user_profile["username"]
+            )
+        )
